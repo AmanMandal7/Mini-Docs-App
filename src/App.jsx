af1542374a38b971abe6351c8ac73b88b3267a8a
Aman Kumar Mandal <aman.mandal.71334@gmail.com>
2024-04-22 17:20:57 +0545

    Done with the background

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='relative w-full h-screen bg-[#2d2d2d]'>
+      <nav className='w-full absolute top-[5%] flex justify-center py-10 text-[20px] font-semibold text-[#1a1a1a]'>Documents</nav>
+      <h1 className='absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] text-[15vw] text-[#131313] leading-none tracking-tighter font-semibold'>Docs.</h1>
+    </div>
   )
 }
 
